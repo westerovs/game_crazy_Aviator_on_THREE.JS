8ad4e5ebf5b1f42b088e04c88d257063cfaf6c72
= <westerovs@mail.ru>
2021-06-16 16:19:20 +0300

    added mouse event

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@
     Объединяем геометрию и материал в меш
     Добавляем меш на нашу сцену
 */
-import * as THREE from '../lib/three.js';
+import * as THREE from '../lib/three.js'
 import {COLORS} from './utils.js'
 import Sky from './world/sky.js'
 import Sea from './world/sea.js'
@@ -29,6 +29,10 @@ class Game {
         this.world       = document.getElementById('fucking-world')
         this.worldWidth  = this.world.offsetWidth
         this.worldHeight = this.world.offsetHeight
+        this.mousePos = {
+            x: 0,
+            y: 0,
+        }
         
         //  1 RENDER
         this.renderer = new THREE.WebGLRenderer({
@@ -38,21 +42,21 @@ class Game {
             // Активируем антиалиасинг. Это может повлечь проблемы с производительностью,
             // но наш проект основан на низкополигональных моделях, поэтому должно зайти как по маслу.
             antialias: true
-        });
+        })
         
         // 2 CAMERA
-        this.fieldOfView = 60;
-        this.aspectRatio = this.worldWidth / this.worldHeight;
-        this.nearPlane   = 1;
-        this.farPlane    = 10000;
+        this.fieldOfView = 60
+        this.aspectRatio = this.worldWidth / this.worldHeight
+        this.nearPlane   = 1
+        this.farPlane    = 10000
         this.camera      = new THREE.PerspectiveCamera(this.fieldOfView, this.aspectRatio, this.nearPlane, this.farPlane)
         
         // 3 SCENE
         this.scene = new THREE.Scene()
         
         this._sky  = new Sky(this.scene)
-        this._sea  = new Sea(this.scene);
-        this._jet  = new Jet(this.scene);
+        this._sea  = new Sea(this.scene)
+        this._jet  = new Jet(this.scene)
         
         // освещение
         this.hemisphereLight = null
@@ -65,32 +69,32 @@ class Game {
         
         // Добавить эффект тумана на нашу сцену
         // его цвет мы возьмем из наших таблиц стилей, а не из переменной
-        this.scene.fog = new THREE.Fog(COLORS.fog, 100, 950);
+        this.scene.fog = new THREE.Fog(COLORS.fog, 100, 950)
         
         // Зададим позицию камере в пространстве
-        this.camera.position.x = 0;
-        this.camera.position.z = 200;
-        this.camera.position.y = 100;
+        this.camera.position.x = 0
+        this.camera.position.z = 200
+        this.camera.position.y = 100
         
         // Задаем размер рендеру,
-        this.renderer.setSize(this.worldWidth, this.worldHeight);
+        this.renderer.setSize(this.worldWidth, this.worldHeight)
         // Включаем рендер теней
-        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.enabled = true
         
         // рендер в html
-        this.world.appendChild(this.renderer.domElement);
+        this.world.appendChild(this.renderer.domElement)
     }
     
+    // цикл для обновления объектов
     loop = () => {
         // Вращаем пропеллер, море и небо
-        this._sea.mesh.rotation.z += .001;
-        this._sky.mesh.rotation.z += .0005;
-        // airplane.propeller.rotation.x += 0.3;
+        this._sea.mesh.rotation.z += .001
+        this._sky.mesh.rotation.z += .0005
+        this._jet.propeller.rotation.x += 0.3
         
         // рендерим сцену
-        this.renderer.render(this.scene, this.camera);
-        
-        requestAnimationFrame(this.loop);
+        this.renderer.render(this.scene, this.camera)
+        requestAnimationFrame(this.loop)
     }
     
     createLights = () => {
@@ -102,29 +106,29 @@ class Game {
         
         // Направленный свет светит в определенном направлении из точки,
         // это работает как солнце, что значит что продуцируемые лучи параллельны.
-        this.shadowLight = new THREE.DirectionalLight(COLORS.sun, .9);
+        this.shadowLight = new THREE.DirectionalLight(COLORS.sun, .9)
         
         // направление света
-        this.shadowLight.position.set(150, 350, 350);
+        this.shadowLight.position.set(150, 350, 350)
         
         // отбрасывание теней
-        this.shadowLight.castShadow = true;
+        this.shadowLight.castShadow = true
         
         // видимая область теней
-        this.shadowLight.shadow.camera.left   = -400;
-        this.shadowLight.shadow.camera.right  = 400;
-        this.shadowLight.shadow.camera.top    = 400;
-        this.shadowLight.shadow.camera.bottom = -400;
-        this.shadowLight.shadow.camera.near   = 1;
-        this.shadowLight.shadow.camera.far    = 1000;
+        this.shadowLight.shadow.camera.left   = -400
+        this.shadowLight.shadow.camera.right  = 400
+        this.shadowLight.shadow.camera.top    = 400
+        this.shadowLight.shadow.camera.bottom = -400
+        this.shadowLight.shadow.camera.near   = 1
+        this.shadowLight.shadow.camera.far    = 1000
         
         // разрешение теней
-        this.shadowLight.shadow.mapSize.width  = 2048;
-        this.shadowLight.shadow.mapSize.height = 2048;
+        this.shadowLight.shadow.mapSize.width  = 2048
+        this.shadowLight.shadow.mapSize.height = 2048
         
         // Добавить освещение на сцену
-        this.scene.add(this.hemisphereLight);
-        this.scene.add(this.shadowLight);
+        this.scene.add(this.hemisphereLight)
+        this.scene.add(this.shadowLight)
     }
     
     // Событие изменения размера экрана,
@@ -136,23 +140,45 @@ class Game {
         const worldWidthResize  = this.world.offsetWidth
         const worldHeightResize = this.world.offsetHeight
         
-        this.renderer.setSize(worldWidthResize, worldHeightResize);
-        this.camera.aspect = worldWidthResize / worldHeightResize;
-        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(worldWidthResize, worldHeightResize)
+        this.camera.aspect = worldWidthResize / worldHeightResize
+        this.camera.updateProjectionMatrix()
     }
     
+    handleMouseMove = (event) => {
+        console.log('x: ', this.mousePos.x)
+        console.log('y: ', this.mousePos.x)
+        console.log(' ')
+    
+        // конвертируем полученные значение положения мыши
+        // в нормализованное значение между -1 и 1
+        // вот формула для горизонтальной оси:
+        let tx = -1 + (event.clientX / this.worldWidth) * 2
+    
+        // для вертикальной оси нам необходимо инвертировать нашу формулу,
+        // так как в 2D y-ось идет в противоположном направлении, в отличии от y-оси в 3D
+    
+        let ty   = 1 - (event.clientY / this.worldHeight) * 2
+        this.mousePos = {
+            x: tx,
+            y: ty
+        }
+    }
+    
+    
     init = () => {
-        this.createScene();  // настройка сцены, камеры и рендера
-        this.createLights(); // добавление освещения сцены
+        this.createScene()  // настройка сцены, камеры и рендера
+        this.createLights() // добавление освещения сцены
         
         // добавление мешей на сцену
-        this._sea.init();
-        this._sky.init();
+        this._sea.init()
+        this._sky.init()
         this._jet.init()
+    
+        this.loop()
         
-        this.loop(); // цикл для обновления объектов
-        
-        window.addEventListener('resize', this.handleWindowResize, false);
+        window.addEventListener('resize', this.handleWindowResize)
+        document.addEventListener('mousemove', this.handleMouseMove)
     }
 }
 
--- js/jet/jet.js
+++ js/jet/jet.js
@@ -1,73 +1,73 @@
 import * as THREE from '../../lib/three.js'
-import { COLORS } from '../utils.js'
+import {COLORS} from '../utils.js'
 
 export default class Jet {
     constructor(scene) {
-        this.mesh = new THREE.Object3D()
+        this.mesh  = new THREE.Object3D()
         this.scene = scene
     }
-
+    
     createCabin = () => {
-        const geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1)
-        const matCockpit = new THREE.MeshPhongMaterial({color:COLORS.red, shading:THREE.FlatShading})
-        const cockpit = new THREE.Mesh(geomCockpit, matCockpit)
+        const geomCockpit = new THREE.BoxGeometry(60, 50, 50, 1, 1, 1)
+        const matCockpit  = new THREE.MeshPhongMaterial({ color: COLORS.red, shading: THREE.FlatShading })
+        const cockpit     = new THREE.Mesh(geomCockpit, matCockpit)
         
-        cockpit.castShadow = true
+        cockpit.castShadow    = true
         cockpit.receiveShadow = true
         this.mesh.add(cockpit)
     }
     
     createEngine = () => {
-        const geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1)
-        const matEngine = new THREE.MeshPhongMaterial({color:COLORS.white, shading:THREE.FlatShading})
-        const engine = new THREE.Mesh(geomEngine, matEngine)
+        const geomEngine = new THREE.BoxGeometry(20, 50, 50, 1, 1, 1)
+        const matEngine  = new THREE.MeshPhongMaterial({ color: COLORS.white, shading: THREE.FlatShading })
+        const engine     = new THREE.Mesh(geomEngine, matEngine)
         
-        engine.position.x = 40
-        engine.castShadow = true
+        engine.position.x    = 40
+        engine.castShadow    = true
         engine.receiveShadow = true
         
         this.mesh.add(engine)
     }
     
     createTail = () => {
-        const geomTailPlane = new THREE.BoxGeometry(15,20,5,1,1,1)
-        const matTailPlane = new THREE.MeshPhongMaterial({color:COLORS.red, shading:THREE.FlatShading})
-        const tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane)
-    
+        const geomTailPlane = new THREE.BoxGeometry(15, 20, 5, 1, 1, 1)
+        const matTailPlane  = new THREE.MeshPhongMaterial({ color: COLORS.red, shading: THREE.FlatShading })
+        const tailPlane     = new THREE.Mesh(geomTailPlane, matTailPlane)
+        
         tailPlane.position.set(-35, 25, 0)
         tailPlane.castShadow    = true
         tailPlane.receiveShadow = true
         
         this.mesh.add(tailPlane)
     }
-
+    
     
     createWing = () => {
-        const geomSideWing = new THREE.BoxGeometry(40,8,150,1,1,1)
-        const matSideWing = new THREE.MeshPhongMaterial({color:COLORS.red, shading:THREE.FlatShading})
-        const sideWing = new THREE.Mesh(geomSideWing, matSideWing)
+        const geomSideWing = new THREE.BoxGeometry(40, 8, 150, 1, 1, 1)
+        const matSideWing  = new THREE.MeshPhongMaterial({ color: COLORS.red, shading: THREE.FlatShading })
+        const sideWing     = new THREE.Mesh(geomSideWing, matSideWing)
         
-        sideWing.castShadow = true
+        sideWing.castShadow    = true
         sideWing.receiveShadow = true
         
         this.mesh.add(sideWing)
     }
     
     createPropeller = () => {
-        const geomPropeller = new THREE.BoxGeometry(20,10,10,1,1,1)
-        const matPropeller = new THREE.MeshPhongMaterial({color:COLORS.brown, shading:THREE.FlatShading})
-
-        this.propeller = new THREE.Mesh(geomPropeller, matPropeller)
-        this.propeller.castShadow = true
+        const geomPropeller = new THREE.BoxGeometry(20, 10, 10, 1, 1, 1)
+        const matPropeller  = new THREE.MeshPhongMaterial({ color: COLORS.brown, shading: THREE.FlatShading })
+        
+        this.propeller               = new THREE.Mesh(geomPropeller, matPropeller)
+        this.propeller.castShadow    = true
         this.propeller.receiveShadow = true
     }
     
     createBlade = () => {
         // лопасть
-        const geomBlade = new THREE.BoxGeometry(1,100,20,1,1,1)
-        const matBlade = new THREE.MeshPhongMaterial({color:COLORS.brownDark, shading:THREE.FlatShading})
-        const blade = new THREE.Mesh(geomBlade, matBlade)
-    
+        const geomBlade = new THREE.BoxGeometry(1, 100, 20, 1, 1, 1)
+        const matBlade  = new THREE.MeshPhongMaterial({ color: COLORS.brownDark, shading: THREE.FlatShading })
+        const blade     = new THREE.Mesh(geomBlade, matBlade)
+        
         blade.position.set(8, 0, 0)
         blade.castShadow    = true
         blade.receiveShadow = true
@@ -88,10 +88,10 @@ export default class Jet {
     
     init = () => {
         console.log('--- create JET ---')
-
-        this.createJet()
         
-        this.mesh.scale.set(0.25,0.25,0.25)
+        this.createJet()
+    
+        this.mesh.scale.set(0.25, 0.25, 0.25)
         this.mesh.position.y = 100
         this.scene.add(this.mesh)
     }
